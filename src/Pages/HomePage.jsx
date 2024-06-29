234d535ca0bc62469d210ef677f0bdf836cab60e
Oyama <godstimeoyama082@gmail.com>
2024-06-29 17:29:13 +0800

    ...

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <div className='text-red-500 text-3xl'>
+    <div className='text-red-500 text-3xl '>
       Homepage
     </div>
   )
