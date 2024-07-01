21f4463231e91c8baad5001413bf8857fd1ef3ec
Peggycarter28 <gpatiencechundung@gmail.com>
2024-07-01 08:58:48 +0100

    Images added to the recidish img file

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function HomePage() {
   return (
-    <div className='text-red-500 text-3xl '>
-      Homepage
+    <div className='text-red-500'>
+ 
     </div>
   )
 }
